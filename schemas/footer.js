880424b51cc12ab2bfd36eeaeed6296d564817f3
Eric Phifer <ericoncode@gmail.com>
2023-04-21 13:32:37 -0600

    add contact and footer schemas for graphql

---
+++ schemas/footer.js
@@ -0,0 +1,39 @@
+import { TbSection as icon } from 'react-icons/tb';
+
+export default {
+    name: 'footer',
+    title: 'Footer Content',
+    type: 'document',
+    icon,
+    fields: [
+        {
+          name: 'title',
+          title: 'Title',
+          type: 'string',
+        },
+        { 
+            name: 'content', 
+            title: 'Content', 
+            type: 'array',
+            of: [{type: 'block'}]
+        },
+        {
+            name: 'image', 
+            title: 'Background Image', 
+            type: 'image',
+            options: {
+                hotspot: true
+            } 
+        },
+        {
+          name: 'alt',
+          title: 'Alternative Text for Image',
+          type: 'string',
+        },
+    ],
+    preview: {
+        select: {
+          title: 'title',
+        },
+      },
+  }
--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,7 @@
 import faqaccordion from './faqaccordion'
 import hero from './hero'
+import contact from './contact'
+import footer from './footer'
 import imagecarousel from './imagecarousel'
 import logos from './logos'
 import navigation from './navigation'
@@ -10,6 +12,8 @@ import PageLinks from '../components/PageLinks'
 export const schemaTypes = [
     faqaccordion,
     hero,
+    contact,
+    footer,
     imagecarousel,
     logos,
     navigation,
