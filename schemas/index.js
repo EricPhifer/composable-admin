2d4f187c231cc9e7bb7f59046649cdd02ed85d2d
Eric Phifer <ericoncode@gmail.com>
2023-05-05 16:17:58 -0600

    add gallery schema

--- schemas/index.js
+++ schemas/index.js
@@ -1,27 +1,43 @@
-import faqaccordion from './faqaccordion'
+// Landing Design
 import hero from './hero'
+// Content Layout
+import faqaccordion from './faqaccordion'
+import gallery from './gallery'
+import section from './section'
+// Forms
 import contact from './contact'
+// Layout Content
+import navigation from './navigation'
 import footer from './footer'
+// Sliders and Carousels
 import imagecarousel from './imagecarousel'
 import logos from './logos'
-import navigation from './navigation'
 import reviews from './reviews'
-import section from './section'
-import privacyPolicy from './privacyPolicy'
-import termsConditions from './termsConditions'
+// Privacy and Terms
+import privacypolicy from './privacypolicy'
+import termsconditions from './termsconditions'
+// Components
 import PageLinks from '../components/PageLinks'
 
 export const schemaTypes = [
-    faqaccordion,
+    // Landing Design
     hero,
+    // Content Layout
+    faqaccordion,
+    section,
+    gallery,
+    // Forms
     contact,
+    // Layout Content
     footer,
+    navigation,
+    // Sliders and Carousels
     imagecarousel,
     logos,
-    navigation,
     reviews,
-    section,
-    privacyPolicy,
-    termsConditions,
+    // Privacy and Terms
+    privacypolicy,
+    termsconditions,
+    // Components
     PageLinks
 ]
